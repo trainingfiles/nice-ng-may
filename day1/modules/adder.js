8416bf66b03dc9f1dc128bd1c05b1130cebc2ab2
Vijay's Training Repo <70316959+trainingfiles@users.noreply.github.com>
2023-05-16 09:04:32 +0530

    Add files via upload

---
+++ day1/modules/adder.js
@@ -0,0 +1,15 @@
+/* export let adder = function(num1, num2){
+    return num1 + num2;
+}; 
+ */
+
+
+let adder = function(num1, num2){
+    return num1 + num2;
+}; 
+let multiplier = function(num1, num2){
+    return num1 * num2;
+}; 
+
+export default adder;
+// export { adder, multiplier };
